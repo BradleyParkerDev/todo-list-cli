a7f359089a51405ab5f3311a2c4fd1fab5deb4fa
BradleyParkerDev <bradleyscottparker@outlook.com>
2022-10-04 15:08:46 -0500

    updated

--- main.js
+++ main.js
@@ -3,15 +3,9 @@ const prompt = require("prompt-sync")({sigint:true});
 //Arrays - these are the list items and completion status 
 const toDoItem = [];
 const toDoStatus = [];
-//const toDoArray = [[],[]];
 
-// Variables
-let item1 = "Go to the dentist";
-let item2 = "Buy groceries";
+// Boolean variable for while loop
 let listOpen = true;
-
-
-
 
 // Initial welcome
 console.log("\nWelcome to the To-Do List Manager Application!\n");
@@ -27,28 +21,20 @@ actionSelection(Number(action));
 while(listOpen){
     if(toDoStatus[0]===  undefined && toDoItem[0] === undefined){
         console.log("Your to-do list is empty.\n");
-
-
     }
     else{
         console.log("\n==============================================\n");
         console.log("You have " + toDoItem.length + " to-do item(s).");
         for(let i = 0; i < toDoItem.length; i++){
             console.log((i+1)+ "." + toDoStatus[i]+ " " + toDoItem[i])
-    
         }
     }
     console.log("\n~ Select an action ~");
     console.log("[1] Create a to-do item");
     console.log("[2] Complete a to-do item");
-
-
-
     let action = prompt("> ");
     actionSelection(Number(action));
-
 }
-
 
 // Creating an item
 function createItem(str){
@@ -59,11 +45,8 @@ function createItem(str){
 
 }
 
-
-
 // Completing an item
 function completeItem(){
- 
     if(toDoStatus[0]===  undefined && toDoItem[0] === undefined){
         console.log("Your list is empty.")
     }else{
@@ -73,8 +56,6 @@ function completeItem(){
         return toDoStatus[i] = "[complete]";
     }
 }
-
-
 
 // Action selection
 function actionSelection(num){
@@ -86,8 +67,6 @@ function actionSelection(num){
     else if(n == 2){    
         completeItem();
     }
-
-
 }
 
 
